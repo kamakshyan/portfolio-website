f6f14bb9400913d5943c0aec4b13c3ec410740f7
Kamakshya Nanda <91257779+kamakshyan@users.noreply.github.com>
2023-03-22 00:44:44 +0530

    changed design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				bounceRight: {
+					'0%,100%': { transform: 'translateX(0)' },
+					'10%, 30%, 50%, 70%, 90%': {transform: 'translateX(-5px)'},
+					'20%, 40%, 60%, 80% ' :{transform: 'translateX(5px)'}
+				  },
+			},
+			animation: {
+				'bounce-right': 'bounceRight 6s ease-in-out infinite',
+			  },
+		},
+		fontFamily: {
+			'montreux': 'Montreux C',
+			'scp': 'Source Code Pro',
+		}
 	},
 	plugins: [],
 }
